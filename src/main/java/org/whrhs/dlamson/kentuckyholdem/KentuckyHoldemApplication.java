c26ba7a2aa31dbfbb1b69ad1759b6f73b6699190
lamsond <dlamson13@gmail.com>
2020-01-04 21:25:40 -0500

    initial commit

---
+++ src/main/java/org/whrhs/dlamson/kentuckyholdem/KentuckyHoldemApplication.java
@@ -0,0 +1,13 @@
+package org.whrhs.dlamson.kentuckyholdem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KentuckyHoldemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KentuckyHoldemApplication.class, args);
+	}
+
+}
